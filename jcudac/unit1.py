931360c7208cf13cd0da601fc3f475ea7963e08b
joshua-curry <joshua.curry@mindbodyonline.com>
2012-11-21 08:10:48 -0800

    Unified Tabbing of Unit1.py

--- jcudac/unit1.py
+++ jcudac/unit1.py
@@ -6,31 +6,31 @@ import webapp2
 
 
 form='''
-<form method = "post">
-	What is your birthday?
-	<br>
-	<lable> Month
-          <input type = "test" name = "month">
-	</lable>
-	<lable> Day
-          <input type = "test" name = "day">
-	</lable>
-	<lable> Year
-          <input type = "test" name = "year">
-	</lable>
-	<input type = "Submit">
-</form>
-'''
+	<form method = "post">
+		What is your birthday?
+		<br>
+		<lable> Month
+			<input type = "test" name = "month">
+		</lable>
+		<lable> Day
+			<input type = "test" name = "day">
+		</lable>
+		<lable> Year
+			<input type = "test" name = "year">
+		</lable>
+		<input type = "Submit">
+	</form>
+	'''
 
 class MainPage(webapp2.RequestHandler):
-  def write_form(self, text=""):
-      self.response.write(Rot13Form %{"text":text})
+	def write_form(self, text=""):
+		self.response.write(Rot13Form %{"text":text})
       
-  def get(self):
-      #self.response.headers['Content-Type'] = 'text/plain'
-      self.response.write(form)
+	def get(self):
+		#self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write(form)
       
-  def post(self):
-      #self.response.headers['Content-Type'] = 'text/plain'
-      self.response.write("Thanks for that totally valid day")
+	def post(self):
+		#self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write("Thanks for that totally valid day")
 
