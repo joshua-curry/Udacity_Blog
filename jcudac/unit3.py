6f4b354e8776cc62defc9807d7f30f630ed6f6ef
joshua-curry <joshua.curry@mindbodyonline.com>
2012-11-21 08:01:51 -0800

    Unified Tabbing of Unit3.py

--- jcudac/unit3.py
+++ jcudac/unit3.py
@@ -7,33 +7,32 @@ from google.appengine.ext import db
 #####################
 
 new_post = '''
-<p>New Post!</p>
-<form method = "post">
-  <lable>Title
-    <input type="text" name="subject" value=%(subject)s>
-  </lable>
-  <br>
-  <lable>Text
-    <textarea name = "content">%(content)s</textarea>
-  </lable>
-  </br>
-  <span style = "color: red">%(error)s</span>
-  </br>
+	<p>New Post!</p>
+	<form method = "post">
+		<lable>Title
+			<input type="text" name="subject" value=%(subject)s>
+		</lable>
+		<br>
+		<lable>Text
+			<textarea name = "content">%(content)s</textarea>
+		</lable>
+		</br>
+		<span style = "color: red">%(error)s</span>
+		</br>
     <input type = "Submit">
-'''
+	'''
 
 frontpage='''
-<html>
-    <head>
-        <title>"Blog"</title>
-    </head>
-
-    <body>
-        <div> %(subject)s </div>
-        <div> %(content)s </div>
-    </body>
-</html>
-'''
+	<html>
+		<head>
+			<title>"Blog"</title>
+		</head>
+		<body>
+			<div> %(subject)s </div>
+			<div> %(content)s </div>
+		</body>
+	</html>
+	'''
 
 def escape_html(s):
     s = s.replace('&', '&amp;')
@@ -43,52 +42,52 @@ def escape_html(s):
     return s
 
 class BlogData(db.Model):
-  subject = db.StringProperty(required = True)
-  content = db.TextProperty(required = True)
-  created = db.DateTimeProperty(auto_now_add = True)
+	subject = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
     
 class blog(webapp2.RequestHandler):
-  def get(self):
-    data = db.GqlQuery('SELECT * FROM BlogData order by created desc')
+	def get(self):
+		data = db.GqlQuery('SELECT * FROM BlogData order by created desc')
     for entry in data:
-      if entry.subject:
-        subject = entry.subject
-      else:
-        subject = ''
+		if entry.subject:
+			subject = entry.subject
+		else:
+			subject = ''
 
-      if entry.content:
-        content = entry.content
-      else:
-        content = ''
+		if entry.content:
+			content = entry.content
+		else:
+			content = ''
         
-      self.response.write(frontpage%{'subject':subject,'content':content})
+		self.response.write(frontpage%{'subject':subject,'content':content})
 
-##  def render_blog(self):
-##      blogs = db.GqlQuery('SElECT * FROM BlogData ORDER BY created DESC ')
-##      self.render('/blog.html', blogs=blogs)
-##
-##  def get(self):
-##      self.render_blog()
+	#def render_blog(self):
+	#	blogs = db.GqlQuery('SElECT * FROM BlogData ORDER BY created DESC ')
+	#	self.render('/blog.html', blogs=blogs)
+
+	#def get(self):
+	#	self.render_blog()
 
 class newpost(webapp2.RequestHandler):
-  def get(self):
-    self.response.write(new_post%{"subject":'',"content":'',"error":''})
+	def get(self):
+		self.response.write(new_post%{"subject":'',"content":'',"error":''})
     
-  def post(self):
-    if self.request.get('subject') != '' and self.request.get('content') != '':
-      data = BlogData(subject = escape_html(self.request.get('subject')), content = escape_html(self.request.get('content')))
-      data.put()
-      dataid = data.key().id()
-      self.redirect('/blog/' + str(dataid))
-    else:
-      error = 'Please enter both fields'
-      self.response.write(new_post%{"subject":escape_html(self.request.get('subject')),"content":escape_html(self.request.get('content')),"error":error})
+	def post(self):
+		if self.request.get('subject') != '' and self.request.get('content') != '':
+			data = BlogData(subject = escape_html(self.request.get('subject')), content = escape_html(self.request.get('content')))
+			data.put()
+			dataid = data.key().id()
+			self.redirect('/blog/' + str(dataid))
+		else:
+			error = 'Please enter both fields'
+			self.response.write(new_post%{"subject":escape_html(self.request.get('subject')),"content":escape_html(self.request.get('content')),"error":error})
   
     
 class blogentry(webapp2.RequestHandler):
-  def get(self, id):
-    self.response.write('blog entry')
-    #self.response.write(BlogData.get_by_id(int(id)))
-    #self.response.write(id)
-    data = BlogData.get_by_id(int(id))
-    self.response.write(data.subject)
+	def get(self, id):
+		self.response.write('blog entry')
+		#self.response.write(BlogData.get_by_id(int(id)))
+		#self.response.write(id)
+		data = BlogData.get_by_id(int(id))
+		self.response.write(data.subject)
